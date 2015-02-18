4d1b85490d0c8e858220d3be378e4b941093dc02
nickto220 <nickto2202006@hotmail.com>
2015-02-19 04:57:39 +0800

    Initial commit

---
+++ app/src/main/java/nick/com/onepiece/MainActivity.java
@@ -0,0 +1,66 @@
+package nick.com.onepiece;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+
+import horizontalscrollview.HorizontalScrollViewAdapter;
+import horizontalscrollview.MyHorizontalScrollView;
+
+
+public class MainActivity extends Activity
+{
+
+    private MyHorizontalScrollView mHorizontalScrollView;
+    private HorizontalScrollViewAdapter mAdapter;
+    private ImageView mImg;
+    private List<Integer> mDatas = new ArrayList<Integer>(Arrays.asList(
+            R.drawable.luffy, R.drawable.luffy2, R.drawable.luffy3, R.drawable.chopper,
+            R.drawable.chopper2, R.drawable.zoro));
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.picking);
+
+        mImg = (ImageView) findViewById(R.id.id_content);
+
+        mHorizontalScrollView = (MyHorizontalScrollView) findViewById(R.id.id_horizontalScrollView);
+        mAdapter = new HorizontalScrollViewAdapter(this, mDatas);
+        //添加滚动回调
+        mHorizontalScrollView
+
+                .setCurrentImageChangeListener(new MyHorizontalScrollView.CurrentImageChangeListener() {
+                    @Override
+                    public void onCurrentImgChanged(int position,
+                                                    View viewIndicator) {
+                        mImg.setImageResource(mDatas.get(position));
+                        viewIndicator.setBackgroundColor(Color
+                                .parseColor("#AA024DA4"));
+                    }
+                });
+        //添加点击回调
+        mHorizontalScrollView.setOnItemClickListener(new MyHorizontalScrollView.OnItemClickListener()
+        {
+
+            @Override
+            public void onClick(View view, int position)
+            {
+                mImg.setImageResource(mDatas.get(position));
+                view.setBackgroundColor(Color.parseColor("#AA024DA4"));
+            }
+        });
+        //设置适配器
+        mHorizontalScrollView.initDatas(mAdapter);
+    }
+
+}
